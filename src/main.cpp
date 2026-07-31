3904b5057c100e5f2506f084459c812ca8bac308
Krix-Official <elbachoide2011@gmail.com>
2026-07-31 18:48:40 -0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,22 +9,19 @@ class $modify(MyMenuLayer, MenuLayer) {
             return false;
         }
 
-        // Icon 50
         auto icon1 = SimplePlayer::create(50);
-        icon1->setPosition({ 100.0f, 100.0f });
-        icon1->setScale(0.8f);
-        icon1->setColor(GameManager::sharedState()->colorForIdx(10));
-        icon1->setSecondColor(GameManager::sharedState()->colorForIdx(5));
+        if (icon1) {
+            icon1->setPosition({ 100.0f, 100.0f });
+            icon1->setScale(0.8f);
+            this->addChild(icon1);
+        }
 
-        // Icon 55
         auto icon2 = SimplePlayer::create(55);
-        icon2->setPosition({ 140.0f, 100.0f });
-        icon2->setScale(0.8f);
-        icon2->setColor(GameManager::sharedState()->colorForIdx(3));
-        icon2->setSecondColor(GameManager::sharedState()->colorForIdx(12));
-
-        this->addChild(icon1);
-        this->addChild(icon2);
+        if (icon2) {
+            icon2->setPosition({ 140.0f, 100.0f });
+            icon2->setScale(0.8f);
+            this->addChild(icon2);
+        }
 
         return true;
     }
